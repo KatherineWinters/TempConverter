32c98ca6e9e27b44e3c4d2d0044d75b3aca85b08
Katherine Winters <goble.katherine@gmail.com>
2022-01-06 10:39:56 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(50);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(10);
+
+            Console.WriteLine($"Celsius after converting from fahrenheit is: {celsius}");
+            Console.WriteLine($"Fahrenheit after converting from celsius is {fahrenheit}");
+
+        }
+    }
+}
